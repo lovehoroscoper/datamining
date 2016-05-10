72b9eca1e41122e5147da016f7668bedd4c89cff
digu <digu@mogujie.com>
2016-05-10 10:27:32 +0800

    update

--- bin/runMergeSample.sh
+++ bin/runMergeSample.sh
@@ -6,9 +6,9 @@ source /home/digu/.bash_profile
 CUR_TIME=`date +%s`
 CUR_DATE=`date  +%Y-%m-%d`
 DAY_SUB1=`date -d "${CUR_DATE} -1 day" +"%Y-%m-%d"`
+DAY_SUB2=`date -d "${CUR_DATE} -2 day" +"%Y-%m-%d"`
 DAY_SUB30=`date -d "${CUR_DATE} -30 day" +"%Y-%m-%d"`
-BIZDATE=${DAY_SUB1}
-BIZDATE_SUB30=${DAY_SUB30}
+BIZDATE=${DAY_SUB2}
 
 #FEATURES="user_item_prefer,user_real_item_prefer,item_ctr,user_shop_prefer,user_shop_prefer_order,user_category_prefer,user_category_prefer_order,user_gene_prefer,user_gene_prefer_order"
 #FEATURES="item_ctr,user_gene_prefer_order,user_gene_prefer"
@@ -16,6 +16,7 @@ FEATURES="${1}"
 #SAMPLE="s_dg_click_sample,s_dg_order_sample,s_dg_all_sample"
 SAMPLE="${2}"
 
+echo "bizdate: ${BIZDATE}"
 echo "features: ${FEATURES}"
 echo "sample: ${SAMPLE}"
 
@@ -33,6 +34,6 @@ ${SUBMIT}														\
 	--executor-memory 7373m										\
 	--class com.mgj.ml.lr.MergeSample							\
 	"${JAR_PATH}"												\
-	"${DAY_SUB1}"												\
+	"${BIZDATE}"												\
 	"${FEATURES}"												\
 	"${SAMPLE}"													\
