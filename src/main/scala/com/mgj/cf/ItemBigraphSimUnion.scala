a772f35f44f2889137b0385923b618ad17cc7301
digu <digu@mogujie.com>
2016-05-12 11:14:31 +0800

    update

--- src/main/scala/com/mgj/cf/ItemBigraphSimUnion.scala
+++ src/main/scala/com/mgj/cf/ItemBigraphSimUnion.scala
@@ -54,10 +54,7 @@ object ItemBigraphSimUnion {
     //      .join(itemInfo).map(x => (x._2._1._1, x._2._1._2, x._2._1._3, x._2._1._4, x._2._2))
     //      .filter(x => x._4.equals(x._5))
 
-    val calendarOutput = Calendar.getInstance()
-    calendarOutput.add(Calendar.DAY_OF_MONTH, -1)
 
-    i2iUnion.map(x => x._1 + " " + x._2 + " " + x._3).saveAsTextFile(outputPath + "/" + sdf.format(calendarOutput.getTime))
     //    i2iFilter.map(x => x._1 + " " + x._2 + " " + x._3).saveAsTextFile(outputPath + "/" + sdf.format(calendarOutput.getTime))
     //    i2i.groupBy(x => x._1).map(x => x._1 + " " + x._2.toList.sortWith((a, b) => a._3 > b._3).map(x => x._2).mkString(" "))
 
@@ -74,10 +71,15 @@ object ItemBigraphSimUnion {
     val max = i2iUnion.map(x => x._3.toDouble).max
     val min = i2iUnion.map(x => x._3.toDouble).min
 
+    val calendarOutput = Calendar.getInstance()
+    calendarOutput.add(Calendar.DAY_OF_MONTH, -1)
+
     i2iUnion.map(x => {
       val score = NormalizeUtil.minMaxScaler(min, max, x._3.toDouble, 1d / const)
       (x._1, x._2, Math.round(score * const))
     }).groupBy(_._1).map(x => x._1 + " " + x._2.map(x => x._2 + ":" + x._3).take(N).mkString(",")).coalesce(2000).saveAsTextFile(outputGroupGlobalNormalizePath + "/" + sdf.format(calendarOutput.getTime))
+
+    i2iUnion.map(x => x._1 + " " + x._2 + " " + x._3).saveAsTextFile(outputPath + "/" + sdf.format(calendarOutput.getTime))
 
     i2iUnion.map(x => (x._1, x._2, x._3.toDouble)).groupBy(_._1).map(x => x._1 + " " + sort(x._2, N)).coalesce(2000).saveAsTextFile(outputGroupPath + "/" + sdf.format(calendarOutput.getTime))
   }
