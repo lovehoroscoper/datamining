24f9db228b537b3c79ead0a4df4fa3e989bb5bce
digu <digu@mogujie.com>
2016-05-12 10:53:05 +0800

    update

--- src/main/scala/com/mgj/cf/ItemBigraphSimUnion.scala
+++ src/main/scala/com/mgj/cf/ItemBigraphSimUnion.scala
@@ -47,16 +47,17 @@ object ItemBigraphSimUnion {
       (x._1._1, x._1._2, score.toString)
     })
 
-    val itemInfo = sqlContext.sql("select tradeitemid, cid from v_dw_trd_tradeitem").rdd.filter(x => x.anyNull == false).map(x => (x(0).toString, x(1).toString))
-    val i2iFilter = i2iUnion.map(x => (x._1, x))
-      .join(itemInfo).map(x => (x._2._1._2, (x._2._1._1, x._2._1._2, x._2._1._3, x._2._2)))
-      .join(itemInfo).map(x => (x._2._1._1, x._2._1._2, x._2._1._3, x._2._1._4, x._2._2))
-      .filter(x => x._4.equals(x._5))
+    //    val itemInfo = sqlContext.sql("select tradeitemid, cid from v_dw_trd_tradeitem").rdd.filter(x => x.anyNull == false).map(x => (x(0).toString, x(1).toString))
+    //    val i2iFilter = i2iUnion.map(x => (x._1, x))
+    //      .join(itemInfo).map(x => (x._2._1._2, (x._2._1._1, x._2._1._2, x._2._1._3, x._2._2)))
+    //      .join(itemInfo).map(x => (x._2._1._1, x._2._1._2, x._2._1._3, x._2._1._4, x._2._2))
+    //      .filter(x => x._4.equals(x._5))
 
     val calendarOutput = Calendar.getInstance()
     calendarOutput.add(Calendar.DAY_OF_MONTH, -1)
 
-    i2iFilter.map(x => x._1 + " " + x._2 + " " + x._3).saveAsTextFile(outputPath + "/" + sdf.format(calendarOutput.getTime))
+    i2iUnion.map(x => x._1 + " " + x._2 + " " + x._3).saveAsTextFile(outputPath + "/" + sdf.format(calendarOutput.getTime))
+    //    i2iFilter.map(x => x._1 + " " + x._2 + " " + x._3).saveAsTextFile(outputPath + "/" + sdf.format(calendarOutput.getTime))
     //    i2i.groupBy(x => x._1).map(x => x._1 + " " + x._2.toList.sortWith((a, b) => a._3 > b._3).map(x => x._2).mkString(" "))
 
     def sort(x: Iterable[(String, String, Double)], N: Int): String = {
@@ -69,12 +70,12 @@ object ItemBigraphSimUnion {
       return list
     }
 
-    i2iFilter.map(x => (x._1, x._2, x._3.toDouble)).groupBy(_._1).map(x => x._1 + " " + sort(x._2, 50)).coalesce(2000).saveAsTextFile(outputGroupPath + "/" + sdf.format(calendarOutput.getTime))
+    i2iUnion.map(x => (x._1, x._2, x._3.toDouble)).groupBy(_._1).map(x => x._1 + " " + sort(x._2, 50)).coalesce(2000).saveAsTextFile(outputGroupPath + "/" + sdf.format(calendarOutput.getTime))
 
-    val max = i2iFilter.map(x => x._3.toDouble).max
-    val min = i2iFilter.map(x => x._3.toDouble).min
+    val max = i2iUnion.map(x => x._3.toDouble).max
+    val min = i2iUnion.map(x => x._3.toDouble).min
 
-    i2iFilter.map(x => {
+    i2iUnion.map(x => {
       val score = NormalizeUtil.minMaxScaler(min, max, x._3.toDouble, 1d / const)
       (x._1, x._2, Math.round(score * const))
     }).groupBy(_._1).map(x => x._1 + " " + x._2.map(x => x._2 + ":" + x._3).mkString(",")).saveAsTextFile(outputGroupGlobalNormalizePath + "/" + sdf.format(calendarOutput.getTime))
