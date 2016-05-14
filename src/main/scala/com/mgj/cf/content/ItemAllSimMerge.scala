583090d7ccaf0ead86f5b9881db38c657a011fcb
digu <digu@mogujie.com>
2016-05-14 14:33:13 +0800

    update

--- src/main/scala/com/mgj/cf/content/ItemAllSimMerge.scala
+++ src/main/scala/com/mgj/cf/content/ItemAllSimMerge.scala
@@ -82,40 +82,41 @@ object ItemAllSimMerge {
     val itemSimMerge = itemBigraphSim.map(x => ((x._1, x._2), x._3)).fullOuterJoin(itemSim.map(x => ((x._1, x._2), x._3)))
       .map(x => (x._1._1, x._1._2, x._2._1.getOrElse(0d), x._2._2.getOrElse(0d)))
 
-    val itemSet = itemSimMerge.map(x => Set(x._1, x._2)).flatMap(x => x).distinct()
-    println(s"item count:${itemSet.count}")
-
-    val itemTitleSeg = sqlContext.sql("select tradeitemid, title from v_dw_trd_tradeitem").map(x => (x(0).toString.toInt, x(1).toString)).join(itemSet.map(x => (x, 1))).map(x => (x._1, x._2._1)).collect()
-      .map(x => (x._1, WordSegUtil.process(x._2).take(50))).toMap
-    itemSet.unpersist(blocking = false)
-
-    WordSegUtil.loadDict(dictPath)
-    val wordTag = sc.textFile(wordTagPath).map(x => (x.split("@")(0), x.split("@")(1))).collect().toMap
-    val wordSim = sc.textFile(wordSimPath).map(x => ((x.split(" ")(0), x.split(" ")(1)), x.split(" ")(2).toDouble)).groupBy(x => x._1._1).map(x => x._2.toList.sortWith((a, b) => a._2 > b._2).take(50)).flatMap(x => x).collect().toMap
-    val wordIdf = sc.textFile(idfPath).map(x => (x.split(" ")(0), x.split(" ")(1).toDouble)).collect().toMap
-
-    val itemSimWithContentTemp = itemSimMerge.map(x => {
-      val itemx = x._1
-      val itemy = x._2
-      val score = GetSimUtil.getSimScore(wordSim, wordTag, wordIdf, itemTitleSeg.get(itemx).get, itemTitleSeg.get(itemy).get)
-      (itemx, itemy, x._3, x._4, score)
-    }).cache()
-    itemSim.unpersist(blocking = false)
-
-    val max = itemSimWithContentTemp.map(x => x._4).max
-    val min = itemSimWithContentTemp.map(x => x._4).min
-
-    val itemSimWithContent = itemSimWithContentTemp.map(x => {
-      val score = (w1 * x._3 + w2 * x._4 + w3 * NormalizeUtil.minMaxScaler(min, max, x._5, 0d)) / (w1 + w2 + w3)
-      (x._1, x._2, score)
-    })
-    itemSimWithContentTemp.unpersist(blocking = false)
-
-    val sdf = new SimpleDateFormat("yyyy-MM-dd")
-    val calendar = Calendar.getInstance()
-    calendar.add(Calendar.DAY_OF_MONTH, -1)
-
-    itemSimWithContent.groupBy(_._1).map(x => x._1 + " " + x._2.toList.sortWith((a, b) => a._3 > b._3).map(x => x._2 + ":" + x._3).mkString(",")).saveAsTextFile(outputPath)
+    //    val itemSet = itemSimMerge.map(x => Set(x._1, x._2)).flatMap(x => x).distinct()
+    //    println(s"item count:${itemSet.count}")
+    //
+    //    val itemTitleSeg = sqlContext.sql("select tradeitemid, title from v_dw_trd_tradeitem").map(x => (x(0).toString.toInt, x(1).toString)).join(itemSet.map(x => (x, 1))).map(x => (x._1, x._2._1)).collect()
+    //      .map(x => (x._1, WordSegUtil.process(x._2).take(50))).toMap
+    //    itemSet.unpersist(blocking = false)
+    //
+    //    WordSegUtil.loadDict(dictPath)
+    //    val wordTag = sc.textFile(wordTagPath).map(x => (x.split("@")(0), x.split("@")(1))).collect().toMap
+    //    val wordSim = sc.textFile(wordSimPath).map(x => ((x.split(" ")(0), x.split(" ")(1)), x.split(" ")(2).toDouble)).groupBy(x => x._1._1).map(x => x._2.toList.sortWith((a, b) => a._2 > b._2).take(50)).flatMap(x => x).collect().toMap
+    //    val wordIdf = sc.textFile(idfPath).map(x => (x.split(" ")(0), x.split(" ")(1).toDouble)).collect().toMap
+    //
+    //    val itemSimWithContentTemp = itemSimMerge.map(x => {
+    //      val itemx = x._1
+    //      val itemy = x._2
+    //      val score = GetSimUtil.getSimScore(wordSim, wordTag, wordIdf, itemTitleSeg.get(itemx).get, itemTitleSeg.get(itemy).get)
+    //      (itemx, itemy, x._3, x._4, score)
+    //    }).cache()
+    //    itemSim.unpersist(blocking = false)
+    //
+    //    val max = itemSimWithContentTemp.map(x => x._4).max
+    //    val min = itemSimWithContentTemp.map(x => x._4).min
+    //
+    //    val itemSimWithContent = itemSimWithContentTemp.map(x => {
+    //      val score = (w1 * x._3 + w2 * x._4 + w3 * NormalizeUtil.minMaxScaler(min, max, x._5, 0d)) / (w1 + w2 + w3)
+    //      (x._1, x._2, score)
+    //    })
+    //    itemSimWithContentTemp.unpersist(blocking = false)
+    //
+    //    val sdf = new SimpleDateFormat("yyyy-MM-dd")
+    //    val calendar = Calendar.getInstance()
+    //    calendar.add(Calendar.DAY_OF_MONTH, -1)
+    //
+    //    itemSimWithContent.groupBy(_._1).map(x => x._1 + " " + x._2.toList.sortWith((a, b) => a._3 > b._3).map(x => x._2 + ":" + x._3).mkString(",")).saveAsTextFile(outputPath)
     //    itemSimMerge.groupBy(_._1).map(x => x._1 + " " + x._2.toList.sortWith((a, b) => a._3 > b._3).map(x => x._2 + ":" + x._3).mkString(" ")).saveAsTextFile(outputPath + "/" + sdf.format(calendar.getTime))
+    itemSimMerge.map(x => (x._1, x._2, (w1 * x._3 + w2 * x._4) / (w1 + w2))).groupBy(_._1).map(x => x._1 + " " + x._2.toList.sortWith((a, b) => a._3 > b._3).map(x => x._2 + ":" + x._3).mkString(",")).saveAsTextFile(outputPath)
   }
 }
