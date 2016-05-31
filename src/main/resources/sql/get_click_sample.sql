f59fef0377ae76f92db45ec5ae367debf2fb44fb
digu <digu@mogujie.com>
2016-05-31 18:59:19 +0800

    update

--- src/main/resources/sql/get_click_sample.sql
+++ src/main/resources/sql/get_click_sample.sql
@@ -12,4 +12,5 @@ WHERE
 	pt = 'BIZDATE'
 	and type = 'click'
 	and platform = 'all'
+	and user_id not in ('19800')
 	and device_id not in ('000000000000000','0000000000000000','Unknown','00000000','0','00000000000000','111111111111111','mgj_2012')
--- src/main/resources/sql/get_order_sample.sql
+++ src/main/resources/sql/get_order_sample.sql
@@ -13,4 +13,5 @@ WHERE
 	pt = 'BIZDATE'
 	and type = 'order'
 	and platform = 'all'
+	and user_id not in ('19800')
 	and device_id not in ('000000000000000','0000000000000000','Unknown','00000000','0','00000000000000','111111111111111','mgj_2012')
