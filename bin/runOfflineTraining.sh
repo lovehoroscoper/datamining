bb5bd7b38024ec7e2df054262b5e9d3e84298441
digu <digu@mogujie.com>
2016-06-06 17:35:30 +0800

    update

--- bin/runOfflineTraining.sh
+++ bin/runOfflineTraining.sh
@@ -17,13 +17,13 @@ YESTERDAY=`date -d "${CUR_DATE} -1 day" +"%Y-%m-%d %H:%M:%S"`
 BIZDATE=${DAY_SUB1}
 echo "bizdate: ${BIZDATE}"
 
-CODE="app_tuan_book_pop"
+CODE="app_clothing_pop"
 echo "code:${CODE}"
 
 # user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos
 #FEATURES="user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos"
 #FEATURES="user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_search_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos"
-FEATURES="user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_search_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos"
+FEATURES="user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos"
 echo "features:${FEATURES}"
 
 SAMPLE_TABLE="s_dg_sample_${CODE}"
@@ -39,7 +39,7 @@ echo "stage:${STAGE}"
 N="20"
 echo "N:${N}"
 
-MODEL_NAME="DIGU_MODEL"
+MODEL_NAME="CLOTHING_MODEL"
 echo "model name:${MODEL_NAME}"
 
 SUBMIT="/home/spark/spark-1.6.0-bin-hadoop2.3/bin/spark-submit "
