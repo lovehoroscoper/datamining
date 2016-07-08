65efedcf622c5eaad676bae88fc787a6d3684095
digu <digu@mogujie.com>
2016-07-08 11:26:40 +0800

    update

--- src/main/scala/com/mgj/feature/impl/udfs/RealSimUdf.scala
+++ src/main/scala/com/mgj/feature/impl/udfs/RealSimUdf.scala
@@ -49,28 +49,29 @@ class RealSimUdf extends UdfTemplate {
         || itemFeature.equals("null")
         || time.equals("null")) {
         0d
-      }
-      val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-      val userClickList = userFeature
-        .split(",")
-        .map(x => x.split("#", 2))
-        .filter(x => x.size == 2).map(x => (x(0), x(1))).filter(x => {
-        sdf.parse(x._2).getTime < sdf.parse(time).getTime
-      })
-      var timeDiff = Long.MaxValue
-      var score = 0d
-      for (x <- userClickList.toIterable) {
-        val list = x._1.split(",").map(x => x.split(":")(0)).take(N).toList
-        val t = list.indexOf(itemFeature) + 1
-        if (t > 0) {
-          timeDiff = sdf.parse(time).getTime - sdf.parse(x._2).getTime
-          val temp = 1.0 / t * 1.0 / (1 + Math.log(1.0 * timeDiff / 1000 / 60 / 10 + 1))
-          if (temp > score) {
-            score = temp
+      } else {
+        val sdf: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val userClickList = userFeature
+          .split(",")
+          .map(x => x.split("#", 2))
+          .filter(x => x.size == 2).map(x => (x(0), x(1))).filter(x => {
+          sdf.parse(x._2).getTime < sdf.parse(time).getTime
+        })
+        var timeDiff = Long.MaxValue
+        var score = 0d
+        for (x <- userClickList.toIterable) {
+          val list = x._1.split(",").map(x => x.split(":")(0)).take(N).toList
+          val t = list.indexOf(itemFeature) + 1
+          if (t > 0) {
+            timeDiff = sdf.parse(time).getTime - sdf.parse(x._2).getTime
+            val temp = 1.0 / t * 1.0 / (1 + Math.log(1.0 * timeDiff / 1000 / 60 / 10 + 1))
+            if (temp > score) {
+              score = temp
+            }
           }
         }
+        score
       }
-      score
     }
     sqlContext.udf.register(name, function)
   }
