eb912bec1dba57bc982861e762aa6d9eead0006c
digu <digu@mogujie.com>
2016-07-13 09:42:21 +0800

    update

--- bin/runOfflineTrainingV2.sh
+++ bin/runOfflineTrainingV2.sh
@@ -27,7 +27,7 @@ FEATURE_TABLE="s_dg_sample_${CODE}"
 echo "feature table:${FEATURE_TABLE}"
 
 # build_sample,adapt_features,train
-STAGE="build_sample,adapt_features,train"
+STAGE="adapt_features"
 echo "stage:${STAGE}"
 
 N="20"
