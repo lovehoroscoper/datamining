7af9173feab21b0ca54f2e2d0e39e9a6cdad6ab2
digu <digu@mogujie.com>
2016-08-07 22:44:20 +0800

    update

--- src/main/resources/sql/userprefer/get_click_log.sql
+++ src/main/resources/sql/userprefer/get_click_log.sql
@@ -9,3 +9,5 @@ where
   and pt <= 'BIZDATESUBB'
   and action_type = 'click'
   and platform_type = 'app'
+  and user_id not in ('19800')
+	and device_id not in ('000000000000000','0000000000000000','Unknown','00000000','0','00000000000000','111111111111111','mgj_2012')
