b3e612b41612aaffc83d4cce189b6d1bed7c6ee9
digu <digu@mogujie.com>
2016-08-09 14:53:27 +0800

    update

--- src/main/scala/com/mgj/feature/impl/udfs/simpleUdf.scala
+++ src/main/scala/com/mgj/feature/impl/udfs/simpleUdf.scala
@@ -18,6 +18,9 @@ class SimpleUdf extends UdfTemplate {
 
   override def register(sqlContext: HiveContext, name: String): Unit = {
     def udf(featureValue: String, scale: String): Double = {
+      if (featureValue == null || featureValue.isEmpty || featureValue.equals("null")) {
+        return 0d
+      }
       return featureValue.toDouble / scale.toDouble
     }
     val function = (featureValue: String, scale: String) => udf(featureValue, scale)
