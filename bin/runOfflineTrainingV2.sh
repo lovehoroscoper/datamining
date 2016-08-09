3df162e103b7bdd5a9ebd908b4746138946c74e9
digu <digu@mogujie.com>
2016-08-09 15:32:50 +0800

    update

--- bin/runOfflineTrainingV2.sh
+++ bin/runOfflineTrainingV2.sh
@@ -18,7 +18,7 @@ echo "code:${CODE}"
 #FEATURES="user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos"
 #FEATURES="user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_search_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos"
 #FEATURES="user_category_prefer,user_category_prefer_order,user_item_prefer,user_real_item_prefer,item_search_ctr,item_ctr,user_shop_prefer,user_shop_prefer_order,user_gene_prefer,user_gene_prefer_order,pos,label"
-FEATURES="user_real_item_prefer,label"
+FEATURES="user_real_item_prefer,item_search_ctr,item_ctr,label"
 echo "features:${FEATURES}"
 
 SAMPLE_TABLE="s_dg_sample_${CODE}_${BIZDATE//-/}"
@@ -41,8 +41,8 @@ ${SUBMIT}														\
 	--master yarn												\
 	--queue root.pool   										\
 	--driver-memory	8g											\
-	--num-executors	16											\
-	--executor-cores 4											\
+	--num-executors	32											\
+	--executor-cores 2											\
 	--executor-memory 7373m										\
 	--class com.mgj.ml.rank.OfflineTrainingV2 		    		\
 	"${JAR_PATH}"												\
