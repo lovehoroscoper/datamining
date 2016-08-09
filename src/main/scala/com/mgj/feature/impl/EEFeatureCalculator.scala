9ac2e9e9e935206b218b63e529dc9a0f84bd147a
digu <digu@mogujie.com>
2016-08-09 14:46:45 +0800

    update

--- src/main/scala/com/mgj/feature/impl/EEFeatureCalculator.scala
+++ src/main/scala/com/mgj/feature/impl/EEFeatureCalculator.scala
@@ -30,6 +30,7 @@ class EEFeatureCalculator extends FeatureCalculator {
 
   override def getFeatureRDD(sc: SparkContext, sqlContext: HiveContext): Seq[(RDD[(String, List[String])], List[String], String)] = {
     val itemEEFeatureDF = sqlContext.sql(s"select cast(tid as string) as ${FeatureConstant.ITEM_KEY}, '0.2' as ${itemField} from online_ee_daily_selection where dt = '${bizDate}' and status = 2 and tid is not null")
+    itemEEFeatureDF.show()
     val result = new util.ArrayList[(RDD[(String, List[String])], List[String], String)]()
     result.add(getFeature(sqlContext, itemEEFeatureDF, FeatureType.ITEM))
     return result.toList.toSeq
