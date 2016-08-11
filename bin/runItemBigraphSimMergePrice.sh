398f0e7a3db6401a937acb11fd9e1e10cb078179
digu <digu@mogujie.com>
2016-08-11 18:19:19 +0800

    update

--- bin/runItemBigraphSimMergePrice.sh
+++ bin/runItemBigraphSimMergePrice.sh
@@ -38,8 +38,9 @@ ${SUBMIT}														\
 	"${ITEM_BIGRAPH_SIM_PATH}"									\
 	"${ITEM_SIM_MERGE_RESULT}"						    	    \
 	"${ITEM_SIM_SEARCH_DUMP_RESULT}"						    \
+	"${PRICE_PATH}"						                        \
 
-${CURL} "http://dc.algo.service.mogujie.org/action/fieldUpdate/doUpdate?id=85"
+#${CURL} "http://dc.algo.service.mogujie.org/action/fieldUpdate/doUpdate?id=85"
 #${CURL} "http://algodc_m.service.mogujie.org/dcAction/execTaskInstance?taskId=51"
 
 #DATA_DIR=${ITEM_SIM_MERGE_RESULT}
